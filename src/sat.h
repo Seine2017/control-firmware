be7f9d8ac86fb181bb51b6e6278551cae14d96c6
Kier Davis <kierdavis@gmail.com>
2017-03-03 16:55:31 +0000

    Make the saturating arithmetic routines pass the tests

--- src/sat.h
+++ src/sat.h
@@ -15,10 +15,10 @@
 // Saturating addition on 16-bit signed numbers.
 static int16_t sat_s16_add(int16_t x, int16_t y) {
   int16_t res = x + y;
-  if (x & y & ~res & 0x8000) {
+  if (~x & ~y & res & 0x8000) {
     return 0x7FFF;
   }
-  else if (~x & ~y & res & 0x8000) {
+  if (x & y & ~res & 0x8000) {
     return 0x8000;
   }
   return res;
@@ -27,10 +27,10 @@ static int16_t sat_s16_add(int16_t x, int16_t y) {
 // Saturating subtraction on 16-bit signed numbers.
 static int16_t sat_s16_sub(int16_t x, int16_t y) {
   int16_t res = x - y;
-  if (x & ~y & ~res & 0x8000) {
+  if (~x & y & res & 0x8000) {
     return 0x7FFF;
   }
-  else if (~x & y & res & 0x8000) {
+  if (x & ~y & ~res & 0x8000) {
     return 0x8000;
   }
   return res;
@@ -38,24 +38,38 @@ static int16_t sat_s16_sub(int16_t x, int16_t y) {
 
 // Saturating multiplication on 16-bit signed numbers.
 static int16_t sat_s16_mul(int16_t x, int16_t y) {
-  //TODO
-  return 0;
+  int32_t res_wide = ((int32_t) x) * ((int32_t) y);
+  if (res_wide > 0x7FFF) {
+    return 0x7FFF;
+  }
+  if (res_wide < -0x8000) {
+    return -0x8000;
+  }
+  return res_wide & 0xFFFF;
 }
 
 // Saturating division on 16-bit signed numbers.
 static int16_t sat_s16_div(int16_t x, int16_t y) {
-  //TODO
-  return 0;
+  // Check for div/0.
+  if (y == 0) {
+    return 0;
+  }
+  // The only situation in which overflow can occur (the result would be 0x8000,
+  // which is unrepresentable).
+  if (x == -0x8000 && y == -0x0001) {
+    return 0x7FFF;
+  }
+  return x / y;
 }
 
 // Saturating addition on 32-bit signed numbers.
 static int32_t sat_s32_add(int32_t x, int32_t y) {
   int32_t res = x + y;
-  if (x & y & ~res & 0x80000000) {
-    return 0x7FFFFFFF;
+  if (~x & ~y & res & 0x80000000L) {
+    return 0x7FFFFFFFL;
   }
-  else if (~x & ~y & res & 0x80000000) {
-    return 0x80000000;
+  if (x & y & ~res & 0x80000000L) {
+    return 0x80000000L;
   }
   return res;
 }
@@ -63,23 +77,37 @@ static int32_t sat_s32_add(int32_t x, int32_t y) {
 // Saturating subtraction on 32-bit signed numbers.
 static int32_t sat_s32_sub(int32_t x, int32_t y) {
   int32_t res = x - y;
-  if (x & ~y & ~res & 0x80000000) {
-    return 0x7FFFFFFF;
+  if (~x & y & res & 0x80000000L) {
+    return 0x7FFFFFFFL;
   }
-  else if (~x & y & res & 0x80000000) {
-    return 0x80000000;
+  if (x & ~y & ~res & 0x80000000L) {
+    return 0x80000000L;
   }
   return res;
 }
 
 // Saturating multiplication on 32-bit signed numbers.
 static int32_t sat_s32_mul(int32_t x, int32_t y) {
-  //TODO
-  return 0;
+  int64_t res_wide = ((int64_t) x) * ((int64_t) y);
+  if (res_wide > 0x7FFFFFFFL) {
+    return 0x7FFFFFFFL;
+  }
+  if (res_wide < -0x80000000L) {
+    return -0x80000000L;
+  }
+  return res_wide & 0xFFFFFFFFL;
 }
 
 // Saturating division on 32-bit signed numbers.
 static int32_t sat_s32_div(int32_t x, int32_t y) {
-  //TODO
-  return 0;
+  // Check for div/0.
+  if (y == 0) {
+    return 0;
+  }
+  // The only situation in which overflow can occur (the result would be
+  // 0x80000000, which is unrepresentable).
+  if (x == -0x80000000L && y == -0x00000001) {
+    return 0x7FFFFFFFL;
+  }
+  return x / y;
 }
