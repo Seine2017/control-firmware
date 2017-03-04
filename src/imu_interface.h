50a912e252a9595e00ab3dac665478531e95a587
Kier Davis <kierdavis@gmail.com>
2017-03-04 17:16:10 +0000

    Improve documentation of raw_readings_t

--- src/imu_interface.h
+++ src/imu_interface.h
@@ -10,13 +10,21 @@
 // are signed 16-bit numbers.
 // TO BE DEFINED: what is the resolution of each of these fields?
 //   i.e. if gyro_x is set to 17, how many degrees/second does that represent?
+// Definition of axes in the body coordinate system:
+//   x axis: points directly between rotors A and B ("forwards")
+//   y axis: points in the direction of thrust ("upwards")
+//   z axis: points directly between rotors B and C ("to the right")
+// Angles:
+//   roll: rotation about the x axis
+//   yaw: rotation about the y axis
+//   pitch: rotation about the z axis
 typedef struct {
-  int16_t accel_x;
-  int16_t accel_y;
-  int16_t accel_z;
-  int16_t gyro_x;
-  int16_t gyro_y;
-  int16_t gyro_z;
+  int16_t accel_x; // Linear acceleration in the x direction.
+  int16_t accel_y; // Linear acceleration in the y direction.
+  int16_t accel_z; // Linear acceleration in the z direction.
+  int16_t gyro_x; // Angular velocity around the x axis (roll velocity).
+  int16_t gyro_y; // Angular velocity around the y axis (yaw velocity).
+  int16_t gyro_z; // Angular velocity around the z axis (pitch velocity).
   int16_t magneto_x;
   int16_t magneto_y;
   int16_t magneto_z;
