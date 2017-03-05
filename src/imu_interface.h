c81290fcc131554c2abd3300e08dc46a178fce1c
Kier Davis <kierdavis@gmail.com>
2017-03-05 14:58:21 +0000

    Rewrite imu_interface.h after today's agreement to move sensor filtering/processing into Pawel's part of the code

--- src/imu_interface.h
+++ src/imu_interface.h
@@ -4,34 +4,35 @@
 #ifndef __IMU_INTERFACE_H
 #define __IMU_INTERFACE_H
 
-#include <stdint.h>
-
-// A structure to hold all of the data that 'read_sensors' retrieves. All fields
-// are signed 16-bit numbers.
-// TO BE DEFINED: what is the resolution of each of these fields?
-//   i.e. if gyro_x is set to 17, how many degrees/second does that represent?
+// A structure to hold all of the data that 'read_sensors' retrieves.
 // Definition of axes in the body coordinate system:
 //   x axis: points directly between rotors A and B ("forwards")
 //   y axis: points in the direction of thrust ("upwards")
 //   z axis: points directly between rotors B and C ("to the right")
-// Angles:
-//   roll: rotation about the x axis
-//   yaw: rotation about the y axis
-//   pitch: rotation about the z axis
 typedef struct {
-  int16_t accel_x; // Linear acceleration in the x direction.
-  int16_t accel_y; // Linear acceleration in the y direction.
-  int16_t accel_z; // Linear acceleration in the z direction.
-  int16_t gyro_x; // Angular velocity around the x axis (roll velocity).
-  int16_t gyro_y; // Angular velocity around the y axis (yaw velocity).
-  int16_t gyro_z; // Angular velocity around the z axis (pitch velocity).
-  int16_t magneto_x;
-  int16_t magneto_y;
-  int16_t magneto_z;
-} raw_readings_t;
+  // Velocity along the world frame Z axis. Positive indicates upwards (away
+  // from the ground). Units are metres/second.
+  float z_vel;
 
-// Read data from the accelerometer, gyroscope and magnetometer, and store the
-// obtained values into the structure pointed to by 'destination'.
-void read_sensors(raw_readings_t *destination);
+  // Roll angle (rotation about the body frame X axis). Positive indicates
+  // rolling to the right (the right-hand side of the vehicle is lower than the
+  // left-hand side). Units are radians.
+  float roll;
+
+  // Pitch angle (rotation about the body frame Y axis). Positive indicates
+  // pitching downwards (the front of the vehicle is lower than the rear). Units
+  // are radians.
+  float pitch;
+
+  // Yaw velocity (angular velocity about the body frame Z axis). Positive
+  // indicates counterclockwise rotation when the vehicle is viewed from above.
+  // Units are radians/second.
+  float yaw_vel;
+} measured_state_t;
+
+// Read data from the accelerometer, gyroscope and possibly magnetometer, filter
+// and process the data, and store the results into the structure pointed to by
+// 'destination'.
+void read_sensors(measured_state_t *destination);
 
 #endif
