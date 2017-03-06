10eab95b6817e24af71af649d2f58186fbd92e9a
Pawel <psk1g14@soton.ac.uk>
2017-03-06 11:14:17 +0000

    Included whole working library for the IMU with Madgwick or Complementary filtering. The filtering method can be chosen by the user.

--- src/imu_interface.h
+++ src/imu_interface.h
@@ -1,8 +1,14 @@
 // This file specifies the interface between the IMU interfacing code (written
 // by Pawel) and the control code (written by Kier).
+// In this file are declared all the necesary function for interfacing 
+// with the IMU. 
 
 #ifndef __IMU_INTERFACE_H
 #define __IMU_INTERFACE_H
+
+// Define macros for the successful or failed communication with IMU
+#define SUCCESS 0
+#define ERROR 1
 
 // A structure to hold all of the data that 'read_sensors' retrieves.
 // Definition of axes in the body coordinate system:
@@ -29,6 +35,20 @@ typedef struct {
   // Units are radians/second.
   float yaw_vel;
 } measured_state_t;
+
+// Variables 
+extern double pitch, roll, pitch_accel, roll_accel;
+
+uint8_t write_IMU_byte(uint8_t IMU_address, uint8_t address, uint8_t data_8bit);
+uint8_t read_IMU_byte(uint8_t IMU_address, uint8_t address, uint8_t *data_8bit);
+uint8_t read_IMU_bytes(uint8_t IMU_address, uint8_t address, uint8_t count, uint8_t * data);
+void read_IMU_id(void);
+void et_IMU_scales(void);
+void calibrate_IMU(void);
+void reset_IMU();
+void init_IMU();
+void read_raw_gyro(uint16_t *gyro);
+void read_raw_accel(uint16_t *accel);
 
 // Read data from the accelerometer, gyroscope and possibly magnetometer, filter
 // and process the data, and store the results into the structure pointed to by
